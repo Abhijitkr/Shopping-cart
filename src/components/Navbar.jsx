59ae218f1d44134024cc8430a9e05caa2ffb5084
AbhijitKr <abhijitkr600@gmail.com>
2024-02-06 23:17:52 +0530

    Fetches products from API

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className=" flex justify-between p-4 bg-green-200 m-5">
+    <div className=" flex justify-between p-4 bg-green-200 my-5">
       <div className="text-3xl font-bold text-green-500">
         <Link to="/">Shopping Cart (Redux)</Link>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,53 @@
+import { useEffect, useState } from "react";
+import { ColorRing } from "react-loader-spinner";
+
 export default function Home() {
-  return <div>Home</div>;
+  const [products, setProducts] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchProducts() {
+    try {
+      setLoading(true);
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      setProducts(data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  console.log(products);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <div className="flex justify-center h-80 items-center">
+          <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="color-ring-loading"
+            wrapperStyle={{}}
+            wrapperClass="color-ring-wrapper"
+            colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+          />
+        </div>
+      ) : (
+        <div className="flex flex-wrap gap-10 justify-between my-10 p-5">
+          {products &&
+            products.length &&
+            products.map((product) => (
+              <div key={product.id} className="">
+                Product Items
+              </div>
+            ))}
+        </div>
+      )}
+    </div>
+  );
 }
