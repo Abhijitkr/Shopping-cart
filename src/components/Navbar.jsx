393029c07f013d57a894cb542816056381bb12d7
AbhijitKr <abhijitkr600@gmail.com>
2024-02-07 18:44:37 +0530

    Adds Products layout with css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className=" flex justify-between p-4 bg-green-200 my-5">
-      <div className="text-3xl font-bold text-green-500">
+    <div className="flex justify-between p-5 bg-green-200 rounded-md fixed container top-0">
+      <div className="text-3xl font-bold text-green-600">
         <Link to="/">Shopping Cart (Redux)</Link>
       </div>
       <ul className="flex gap-8 text-xl font-semibold mr-4">
-        <li>
+        <li className="hover:text-gray-600">
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li className="hover:text-gray-600">
           <Link to="/cart">Cart</Link>
         </li>
       </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,12 +38,27 @@ export default function Home() {
           />
         </div>
       ) : (
-        <div className="flex flex-wrap gap-10 justify-between my-10 p-5">
+        <div className="flex flex-wrap gap-10 justify-center my-24 p-5">
           {products &&
             products.length &&
             products.map((product) => (
-              <div key={product.id} className="">
-                Product Items
+              <div
+                key={product.id}
+                className="w-48 border-2 border-green-500 p-5 flex flex-col justify-center align-center text-center gap-5"
+              >
+                <img
+                  src={product.image}
+                  alt={product.description}
+                  className="max-h-40"
+                />
+
+                <div>
+                  <h3 className="truncate capitalize">{product.description}</h3>
+                  <p className="text-lg font-semibold">${product.price}</p>
+                  <button className="bg-green-500 py-2 px-4 text-white mt-2 font-semibold hover:bg-green-400 active:scale-50 duration-300">
+                    Add to Cart
+                  </button>
+                </div>
               </div>
             ))}
         </div>
